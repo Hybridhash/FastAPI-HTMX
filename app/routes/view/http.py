7f525ab6aeb00967b471addfd87f06b734fac410
MAhmadAzhar <mahmadazhar@outlook.com>
2023-10-17 22:34:05 +0300

    feat: :sparkles: HTTP for login is provided

--- app/routes/view/http.py
+++ app/routes/view/http.py
@@ -1,17 +1,17 @@
 import httpx
 
 
-async def get_login(username: str, password: str) -> httpx.Response:
+# Creating a middleware for the fastapi user to handle the errors in backend 
+# Response will be provided to HTML response model in view for rendering the page
+async def get_login_http(username: str, password: str) -> httpx.Response:
     # Create an HTTP client
     client = httpx.AsyncClient()
-
     try:
         # Make the HTTP request with the credentials
         response = await client.post(
             "http://localhost:8080/auth/jwt/login",
             data={"grant_type": "", "username": username, "password": password, "scope": "", "client_id": "", "client_secret": ""},
         )
-
         # Check the response status code
         if response.status_code == 204:
             print("Login successful!")
@@ -24,8 +24,6 @@ async def get_login(username: str, password: str) -> httpx.Response:
             print(f"Error: {response.status_code} - {response.text}")
 
         return response
-
     finally:
         # Close the HTTP client
-        await client.aclose()        # Close the HTTP client
         await client.aclose()
--- app/routes/view/login.py
+++ app/routes/view/login.py
@@ -1,13 +1,15 @@
 
+import http
 from typing import Optional
 
 from fastapi import Depends, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import HTMLResponse, RedirectResponse, Response
 from fastapi.routing import APIRouter
 from loguru import logger
 
 from app.database.security import current_active_user, verify_jwt
 from app.models.users import User as UserModelDB
+from app.routes.view.http import get_login_http
 from app.templates import templates
 
 # Create an APIRouter
@@ -52,11 +54,49 @@ async def get_login(request: Request, invalid: Optional[bool] = None, logged_out
 
 # Defining a route for the login post request
 @login_view_route.post("/login", summary="Logs in a user", tags=["Pages","Authentication"])
-async def post_login(request: Request):
+async def post_login(request: Request, response:Response) -> RedirectResponse:
     logger.info(request)
     form = await request.form()
     username = form.get("username")
     password = form.get("password")
-    logger.info(username)
-    logger.info(password)
-    return RedirectResponse('/dashboard', status_code=302)
+    logger.debug(username)
+    logger.debug(password)
+    response = RedirectResponse("/", status_code=302)
+    await set_cookie(str(form.get("username")), str(form.get("password")),response)
+    return response
+
+        
+async def set_cookie(username:str, password:str, response:Response) -> None:
+    # logger.info(request)
+    # form = await request.form()
+    # username = form.get("username")
+    # password = form.get("password")
+    # logger.debug(username)
+    # logger.debug(password)
+
+    response.set_cookie("fastapiusersauth", "test", max_age=3600)
+    httpx_response = await get_login_http(username, password)
+    if httpx_response is not None:
+        cookie = http.cookies.SimpleCookie(httpx_response.headers["set-cookie"])["fastapiusersauth"]
+        logger.debug(cookie)
+        cookie_value = cookie.value
+        logger.debug(cookie_value)
+        cookie_httponly = cookie["httponly"]
+        logger.debug(cookie_httponly)
+        cookie_max_age = cookie["max-age"]
+        logger.debug(cookie_max_age)
+        cookie_path = cookie["path"]
+        logger.debug(cookie_path)
+        cookie_samesite = cookie["samesite"]
+        logger.debug(cookie_samesite)
+        
+        response.set_cookie(
+        key="fastapiusersauth",
+        value=cookie_value,
+        httponly=cookie_httponly,
+        max_age=cookie_max_age,
+        path=cookie_path,
+        samesite=cookie_samesite,
+        secure=True)
+        logger.debug(response.headers)
+

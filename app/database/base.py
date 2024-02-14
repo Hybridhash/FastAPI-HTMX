94bd639b70926bede3f352b0d177742e19161e43
MAhmadAzhar <mahmadazhar@outlook.com>
2024-02-14 08:38:44 +0300

    feat: :sparkles: Add Alembic migration files and amended base implementation

--- app/database/base.py
+++ app/database/base.py
@@ -1,7 +1,35 @@
+import os
+import sys
 
+import sqlalchemy
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+sys.path.append(BASE_DIR)
+
+
+class Base(DeclarativeBase):
+    metadata: sqlalchemy.MetaData = sqlalchemy.MetaData()  # type: ignore
+
+
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+
+engine = create_async_engine(DATABASE_URL, echo=False)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+def init_models():
+    from ..models.groups import Group, Permission, UserGroupLink  # noqa: F401
+    from ..models.users import Role, User, UserActivity  # noqa: F401
+
+
+# Base = declarative_base()
 
 
 # Declaring the base class for all models
-class Base(DeclarativeBase):
-    pass
+# class Base(DeclarativeBase):
+#     pass

0d3fb9799a181118dd4d2e70ebace9860c2d12ad
MAhmadAzhar <mahmadazhar@outlook.com>
2024-02-19 08:43:30 +0300

    fix: :bug: Add edit_group.html template and update groups.html template
    Add edit_group.html template and update groups.html template.

--- app/routes/view/group.py
+++ app/routes/view/group.py
@@ -1,3 +1,5 @@
+import uuid
+
 import nh3
 from fastapi import Depends, HTTPException, Request, Response
 from fastapi.responses import HTMLResponse
@@ -12,8 +14,6 @@ from app.models.users import User as UserModelDB
 from app.routes.view.view_crud import SQLAlchemyCRUD
 from app.schema.group import GroupCreate
 from app.templates import templates
-
-# Creating a group API route
 
 group_view_route = APIRouter()
 
@@ -52,10 +52,31 @@ async def get_create_group(
     # checking the current user as super user
     if not current_user.is_superuser:
         raise HTTPException(status_code=403, detail="Not authorized to add groups")
-    # Redirecting to the add role page upon successful role creation
+    # Redirecting to the add group page upon successful group creation
     return templates.TemplateResponse(
         "partials/add_group.html",
         {"request": request},
+    )
+
+
+# Route to get the group by ID
+@group_view_route.get("/get_group/{group_id}", response_class=HTMLResponse)
+async def get_group_by_id(
+    request: Request,
+    group_id: uuid.UUID,
+    db: CurrentAsyncSession,
+    current_user: UserModelDB = Depends(current_active_user),
+):
+    # checking the current user as super user
+    if not current_user.is_superuser:
+        raise HTTPException(status_code=403, detail="Not authorized to add groups")
+    group = await group_crud.read_by_primary_key(db, group_id)
+    return templates.TemplateResponse(
+        "partials/edit_group.html",
+        {
+            "request": request,
+            "group": group,
+        },
     )
 
 
@@ -68,7 +89,7 @@ async def post_create_group(
 ):
     # checking the current user as super user
     if not current_user.is_superuser:
-        raise HTTPException(status_code=403, detail="Not authorized to add roles")
+        raise HTTPException(status_code=403, detail="Not authorized to add groups")
 
     try:
         form = await request.form()
@@ -87,10 +108,10 @@ async def post_create_group(
             raise HTTPException(status_code=400, detail="Group name already exists")
         await group_crud.create(dict(group_create), db)
 
-        # Redirecting to the add role page upon successful role creation
+        # Redirecting to the add group page upon successful group creation
         headers = {
-            "HX-Location": "/role",
-            "HX-Trigger": "roleAdded",
+            "HX-Location": "/groups",
+            "HX-Trigger": "groupAdded",
         }
 
         return HTMLResponse(content="", headers=headers)
@@ -121,3 +142,80 @@ async def post_create_group(
                 "error_messages": ["An unexpected error occurred: {}".format(e)],
             },
         )
+
+
+# Route to update a group
+@group_view_route.put("/post_update_group{group_id}", response_class=HTMLResponse)
+async def post_update_group(
+    request: Request,
+    response: Response,
+    group_id: uuid.UUID,
+    db: CurrentAsyncSession,
+    current_user: UserModelDB = Depends(current_active_user),
+):
+    # checking the current user as super user
+    if not current_user.is_superuser:
+        raise HTTPException(status_code=403, detail="Not authorized to add groups")
+
+    try:
+        form = await request.form()
+
+        # Iterate over the form fields and sanitize the values before validating against the Pydantic model
+        group_update = GroupCreate(
+            group_name=nh3.clean(str(form.get("group_name"))),
+            group_desc=nh3.clean(str(form.get("group_desc"))),
+        )
+
+        await group_crud.update(db, group_id, dict(group_update))
+
+        # Redirecting to the add group page upon successful group creation
+        headers = {
+            "HX-Location": "/groups",
+            "HX-Trigger": "groupUpdated",
+        }
+        return HTMLResponse(content="", headers=headers)
+    except ValidationError as e:
+        logger.debug(e.errors())
+        return templates.TemplateResponse(
+            "partials/edit_group.html",
+            {
+                "request": request,
+                "group": await group_crud.read_by_primary_key(db, group_id),
+                "error_messages": [
+                    f"{str(error['loc']).strip('(),')}: {error['msg']}"
+                    for error in e.errors()
+                ],
+            },
+        )
+    except HTTPException as e:
+        return templates.TemplateResponse(
+            "partials/edit_group.html",
+            {"request": request, "error_messages": [e.detail]},
+        )
+    except Exception as e:
+        return templates.TemplateResponse(
+            "partials/edit_group.html",
+            {
+                "request": request,
+                "error_messages": ["An unexpected error occurred: {}".format(e)],
+            },
+        )
+
+
+# Route to delete a group
+@group_view_route.delete("/delete_group/{group_id}", response_class=HTMLResponse)
+async def delete_group(
+    request: Request,
+    group_id: uuid.UUID,
+    db: CurrentAsyncSession,
+    current_user: UserModelDB = Depends(current_active_user),
+):
+    # checking the current user as super user
+    if not current_user.is_superuser:
+        raise HTTPException(status_code=403, detail="Not authorized to add groups")
+    await group_crud.delete(db, group_id)
+    headers = {
+        "HX-Location": "/groups",
+        "HX-Trigger": "groupDeleted",
+    }
+    return HTMLResponse(content="", headers=headers)

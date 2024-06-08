5054584f4e46aa38fcc92213741fb8fba063295d
MAhmadAzhar <mahmadazhar@outlook.com>
2024-06-08 22:40:57 +0300

    refactor: Update templates to display user type based on current user's superuser status
    The code changes in this commit update the templates to display different navigation options based on the current user's superuser status. The `auth_navigation.html` template now includes conditional rendering of navigation links based on the `user_type` variable, which is set to `True` if the current user is a superuser. This enhancement improves the user experience by providing relevant navigation options based on the user's role.

--- app/routes/view/login.py
+++ app/routes/view/login.py
@@ -29,6 +29,7 @@ async def get_dashboard(
             "title": "My Web Page",
             "message": f"Welcome to my web page!{user.email}",
             "cookie_value": cookie_value,
+            "user_type": user.is_superuser,
         },
     )
 
